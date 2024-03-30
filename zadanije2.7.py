d4cb5a717a4581373e5d43b76e38cabc265ad8a4
Jusieee <Smrtv.a@gmail.com>
2024-03-30 16:08:52 +0300

    Посчитал общее количесвто 3 песен в минутах

---
+++ zadanije2.7.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+violator_songs_list = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
+
+Songs_1 = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[7][1]
+
+print('Три песни звучат', Songs_1, 'минут.')
+
