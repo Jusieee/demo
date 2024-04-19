cd4f15a4040c492508ea87e86eb1d90384fbb53f
Jusieee <Smrtv.a@gmail.com>
2024-04-19 14:46:04 +0300

    Создал функцию для создания круга

--- zadanije 3.1.py
+++ zadanije 3.1.py
@@ -10,4 +10,11 @@ for _ in range(3):
     radius += 5
     sd.circle(center_position=point, radius=radius)
 
+def bubble(point, step):
+    radius = step
+    sd.circle(center_position=point, radius=radius)
+
+point = sd.get_point(500, 500)
+bubble(point=point, step=20)
+
 sd.pause()
