1026181cc377e8c4522d7e2deed6fdd9e51b6994
Jusieee <Smrtv.a@gmail.com>
2024-04-23 14:15:33 +0300

    Проверка размеров входа

---
+++ zadanije 3.3.py
@@ -0,0 +1,60 @@
+# -*-  coding: utf-8 -*-
+
+envelop_x, envelop_y = 10, 7
+paper_x, paper_y = 8, 9
+
+x, y = envelop_x, envelop_y
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
+
+paper_x, paper_y = 9, 8
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
+
+paper_x, paper_y = 6, 8
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
+
+paper_x, paper_y = 8, 6
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
+
+paper_x, paper_y = 3, 4
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
+
+paper_x, paper_y = 11, 9
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
+
+paper_x, paper_y = 9, 11
+z, v = paper_x, paper_y
+
+if x >= z and y >= v:
+    print('ДА')
+else:
+    print('НЕТ')
